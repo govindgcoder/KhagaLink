cf754a196af591a24d1efd67155ba13a748f81cb
govind <govind.g.coder@gmail.com>
2026-01-16 18:12:49 +0530

    added ui elements for project dashboard

--- src/components/csvVisualizer.tsx
+++ src/components/csvVisualizer.tsx
@@ -3,251 +3,222 @@ import { useProjectStore } from "../stores/useStore";
 import { open } from "@tauri-apps/plugin-dialog";
 
 export default function CsvVisualizer() {
-	const [path, setPath] = useState("");
-	const [offset, setOffset] = useState(0);
-	const PAGE_SIZE = 50;
+    const [path, setPath] = useState("");
+    const [offset, setOffset] = useState(0);
+    const PAGE_SIZE = 50;
 
-	const metadata = useProjectStore((state) => state.currentCSVmetadata);
+    const metadata = useProjectStore((state) => state.currentCSVmetadata);
 
-	const loadCSVmetadata = useProjectStore((state) => state.loadCSVmetadata);
+    const loadCSVmetadata = useProjectStore((state) => state.loadCSVmetadata);
 
-	const currentProject = useProjectStore((state) => state.current_project);
+    const currentProject = useProjectStore((state) => state.current_project);
 
-	const currentCSVrows = useProjectStore((state) => state.currentCSVrows);
+    const currentCSVrows = useProjectStore((state) => state.currentCSVrows);
 
-	const loadCSVrows = useProjectStore((state) => state.loadCSVrows);
+    const loadCSVrows = useProjectStore((state) => state.loadCSVrows);
 
-	const loadView = useProjectStore((state) => state.load_view);
+    const addCsvToList = useProjectStore((state) => state.addCsvToList);
 
-	const addCsvToList = useProjectStore((state) => state.addCsvToList);
+    const delCsvFromList = useProjectStore((state) => state.delCsvFromList);
 
-	const delCsvFromList = useProjectStore((state) => state.delCsvFromList);
+    // if (!metadata) {
+    // 	return <div>No CSV loaded</div>;
+    // }
 
-	// if (!metadata) {
-	// 	return <div>No CSV loaded</div>;
-	// }
+    const handleView = () => {
+        if (!path) return;
+        addCsvToList(path);
+        loadCSVmetadata(path);
+        loadCSVrows(path, 0, 50);
+    };
 
-	const handleView = () => {
-		if (!path) return;
-		addCsvToList(path);
-		loadCSVmetadata(path);
-		loadCSVrows(path, 0, 50);
-	};
+    //to handle pagination
+    const handleNext = () => {
+        const newOffset = offset + PAGE_SIZE;
+        // Check if at the end
+        if (metadata && newOffset < metadata.total_rows) {
+            setOffset(newOffset);
+            loadCSVrows(path, newOffset, PAGE_SIZE);
+        }
+    };
 
-	const handleExit = () => {
-		loadView("Home");
-	};
+    const handlePrev = () => {
+        const newOffset = Math.max(0, offset - PAGE_SIZE); // to never go below 0
+        setOffset(newOffset);
+        loadCSVrows(path, newOffset, PAGE_SIZE);
+    };
 
-	//to handle pagination
-	const handleNext = () => {
-		const newOffset = offset + PAGE_SIZE;
-		// Check if at the end
-		if (metadata && newOffset < metadata.total_rows) {
-			setOffset(newOffset);
-			loadCSVrows(path, newOffset, PAGE_SIZE);
-		}
-	};
+    const handleBrowse = async () => {
+        try {
+            const selected = await open({
+                directory: false,
+                multiple: false,
+                title: "Select a csv file",
+            });
+            if (selected) {
+                setPath(selected);
+            }
+        } catch (err) {
+            console.log("Failed to open dialog: ", err);
+        }
+    };
+    // update table when another file is selected
+    useEffect(() => {
+        if (path) {
+            handleView();
+            setOffset(0);
+        }
+    }, [path]);
 
-	const handlePrev = () => {
-		const newOffset = Math.max(0, offset - PAGE_SIZE); // to never go below 0
-		setOffset(newOffset);
-		loadCSVrows(path, newOffset, PAGE_SIZE);
-	};
+    return (
+        <div
+            className="p-8 flex flex-col gap-4 h-full bg-[var(--background-color)] rounded-xl "
+            style={{ minWidth: "800px" }}
+        >
+            <h2 className="text-xl font-semibold mb-4">CSV Analysis</h2>
+            <div className="flex gap-2 ">
+                <input
+                    type="text"
+                    value={path}
+                    hidden
+                    readOnly
+                    placeholder="Select a folder..."
+                />
+                <button
+                    onClick={handleBrowse}
+                    style={{
+                        backgroundColor: "#007BFF",
+                        color: "white",
+                        padding: "10px 20px",
+                        border: "none",
+                        borderRadius: "4px",
+                        cursor: "pointer",
+                        fontFamily: "'Arial', sans-serif",
+                    }}
+                >
+                    Add New CSV
+                </button>
+            </div>
 
-	const handleBrowse = async () => {
-		try {
-			const selected = await open({
-				directory: false,
-				multiple: false,
-				title: "Select a csv file",
-			});
-			if (selected) {
-				setPath(selected);
-			}
-		} catch (err) {
-			console.log("Failed to open dialog: ", err);
-		}
-	};
-	// update table when another file is selected
-	useEffect(() => {
-		if (path) {
-			handleView();
-			setOffset(0);
-		}
-	}, [path]);
+            <div>
+                {currentProject?.csv_files.map((file, index) => {
+                    return (
+                        <div
+                            key={index}
+                            className={`m-4 p-4 flex ${file.path === path ? "bg-blue-500 text-white" : "bg-blue-300"}`}
+                        >
+                            <button
+                                className="w-full"
+                                onClick={() => setPath(file.path)}
+                            >
+                                {file.path.replace(/^.*[\\\/]/, "")}
+                            </button>
+                            <button
+                                onClick={() => delCsvFromList(file.path)}
+                                className="bg-red-500 px-3 py-1 rounded font-mono"
+                            >
+                                Delete
+                            </button>
+                        </div>
+                    );
+                })}
+            </div>
 
-	return (
-		<div
-			className="p-8 flex flex-col gap-4 h-full bg-[var(--background-color)] rounded-xl "
-			style={{ minWidth: "800px" }}
-		>
-			<div className="flex justify-between">
-				<p className="text-3xl">
-					{currentProject !== null
-						? currentProject.name
-						: "No Project"}
-				</p>
-				<button
-					onClick={handleExit}
-					className="size-fit px-2 bg-red-400 rounded-full font-light"
-				>
-					<p className="text-2xl">X</p>
-				</button>
-			</div>
-			
-			<h2
-				className="text-xl font-bold mb-4"
-				style={{
-					fontFamily: "'Arial', sans-serif",
-					fontSize: "24px",
-					color: "#333",
-				}}
-			>
-				CSV Analysis
-			</h2>
-			<div className="flex gap-2 m-4">
-				<input
-					type="text"
-					value={path}
-					hidden
-					readOnly
-					placeholder="Select a folder..."
-				/>
-				<button
-					onClick={handleBrowse}
-					style={{
-						backgroundColor: "#007BFF",
-						color: "white",
-						padding: "10px 20px",
-						border: "none",
-						borderRadius: "4px",
-						cursor: "pointer",
-						fontFamily: "'Arial', sans-serif",
-					}}
-				>
-					Add New CSV
-				</button>
-			</div>
+            <p
+                className="mt-2"
+                style={{
+                    fontFamily: "'Arial', sans-serif",
+                    fontSize: "16px",
+                    color: "#555",
+                }}
+            >
+                Total Rows: {metadata == null ? 0 : metadata.total_rows} |
+                Columns: {metadata ? metadata.headers.length : 0}
+            </p>
 
-			<div>
-				{currentProject?.csv_files.map((file, index) => {
-					return (
-						<div
-							key={index}
-							className={`m-4 p-4 flex ${file.path === path ? "bg-blue-500 text-white" : "bg-blue-300"}`}
-						>
-							<button
-								className="w-full"
-								onClick={() => setPath(file.path)}
-							>
-								{file.path.replace(/^.*[\\\/]/, "")}
-							</button>
-							<button
-								onClick={() => delCsvFromList(file.path)}
-								className="bg-red-500 px-3 py-1 rounded font-mono"
-							>
-								Delete
-							</button>
-						</div>
-					);
-				})}
-			</div>
-
-			<p
-				className="mt-2"
-				style={{
-					fontFamily: "'Arial', sans-serif",
-					fontSize: "16px",
-					color: "#555",
-				}}
-			>
-				Total Rows: {metadata == null ? 0 : metadata.total_rows} |
-				Columns: {metadata ? metadata.headers.length : 0}
-			</p>
-
-			{/*<ul className="list-disc pl-5">
+            {/*<ul className="list-disc pl-5">
 				{metadata
 					? metadata.headers.map((header, index) => (
 							<li key={index}>{header}</li>
 						))
 					: null}
 			</ul>*/}
-			<div style={{ display: "flex", justifyContent: "space-between" }}>
-				<button
-					onClick={handlePrev}
-					style={{
-						backgroundColor: "#007BFF",
-						color: "white",
-						padding: "10px 20px",
-						border: "none",
-						borderRadius: "4px",
-						cursor: "pointer",
-						fontFamily: "'Arial', sans-serif",
-					}}
-				>
-					Previous
-				</button>
-				<button
-					onClick={handleNext}
-					style={{
-						backgroundColor: "#007BFF",
-						color: "white",
-						padding: "10px 20px",
-						border: "none",
-						borderRadius: "4px",
-						cursor: "pointer",
-						fontFamily: "'Arial', sans-serif",
-					}}
-				>
-					Next
-				</button>
-			</div>
-			<div
-				className="flex overflow-y-auto h-full"
-				style={{
-					maxHeight: "70vh",
-					overflow: "auto",
-					border: "1px solid black",
-					borderRadius: "4px",
-					padding: "8px",
-				}}
-			>
-				<table style={{ borderCollapse: "collapse", width: "100%" }}>
-					<thead>
-						<tr>
-							{metadata?.headers.map((header, index) => (
-								<th
-									key={index}
-									style={{
-										border: "1px solid black",
-										padding: "8px",
-										textAlign: "left",
-									}}
-								>
-									{header}
-								</th>
-							))}
-						</tr>
-					</thead>
-					<tbody>
-						{currentCSVrows?.map((row, index) => (
-							<tr key={index}>
-								{row.map((cell, index) => (
-									<td
-										key={index}
-										style={{
-											border: "1px solid black",
-											padding: "8px",
-										}}
-									>
-										{cell}
-									</td>
-								))}
-							</tr>
-						))}
-					</tbody>
-				</table>
-			</div>
-		</div>
-	);
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <button
+                    onClick={handlePrev}
+                    style={{
+                        backgroundColor: "#007BFF",
+                        color: "white",
+                        padding: "10px 20px",
+                        border: "none",
+                        borderRadius: "4px",
+                        cursor: "pointer",
+                        fontFamily: "'Arial', sans-serif",
+                    }}
+                >
+                    Previous
+                </button>
+                <button
+                    onClick={handleNext}
+                    style={{
+                        backgroundColor: "#007BFF",
+                        color: "white",
+                        padding: "10px 20px",
+                        border: "none",
+                        borderRadius: "4px",
+                        cursor: "pointer",
+                        fontFamily: "'Arial', sans-serif",
+                    }}
+                >
+                    Next
+                </button>
+            </div>
+            <div
+                className="flex overflow-y-auto h-full"
+                style={{
+                    maxHeight: "70vh",
+                    overflow: "auto",
+                    border: "1px solid black",
+                    borderRadius: "4px",
+                    padding: "8px",
+                }}
+            >
+                <table style={{ borderCollapse: "collapse", width: "100%" }}>
+                    <thead>
+                        <tr>
+                            {metadata?.headers.map((header, index) => (
+                                <th
+                                    key={index}
+                                    style={{
+                                        border: "1px solid black",
+                                        padding: "8px",
+                                        textAlign: "left",
+                                    }}
+                                >
+                                    {header}
+                                </th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {currentCSVrows?.map((row, index) => (
+                            <tr key={index}>
+                                {row.map((cell, index) => (
+                                    <td
+                                        key={index}
+                                        style={{
+                                            border: "1px solid black",
+                                            padding: "8px",
+                                        }}
+                                    >
+                                        {cell}
+                                    </td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
 }
--- src/components/projectDashboard.tsx
+++ src/components/projectDashboard.tsx
@@ -1,49 +1,145 @@
 import { useState, useEffect } from "react";
 import CsvVisualizer from "./csvVisualizer";
+import { useProjectStore } from "../stores/useStore";
 
 export default function ProjectDashboard() {
-	const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
-	useEffect(() => {
-		function onKeyDown(e: KeyboardEvent) {
-			if (e.key === "Escape") {
-				setIsSidebarOpen(false);
-			}
-		}
-		window.addEventListener("keydown", onKeyDown);
-		return () => window.removeEventListener("keydown", onKeyDown);
-	}, []);
+    const currentProject = useProjectStore((state) => state.current_project);
+    const loadView = useProjectStore((state) => state.load_view);
 
-	const handleToggleSideBar = () => {
-		setIsSidebarOpen(!isSidebarOpen);
-	};
+    const [selectedTab, setSelectedTab] = useState("csv");
 
-	return (
-		<div className="size-full">
-			<div
-				className="project-dashboard-layout bg-[var(--secondary-color)] p-4 w-screen h-screen flex"
-			>
-				<div id="sidebar" className={`h-full ${isSidebarOpen ? 'w-42' : 'w-12'} bg-[var(--secondary-color)] transition-all mr-3`}>
-					<div className="flex flex-col justify-between h-full">
-						<div className="flex items-center justify-between py-2">
-							<button onClick={handleToggleSideBar}>
-								{isSidebarOpen ? (
-									<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-										<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
-									</svg>
-								) : (
-									<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-										<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6l6 6-6 6" />
-									</svg>
-								)}
-							</button>
-						</div>
-					</div>
-				</div>
-				<div id="main-csv-content" className="w-full bg-[var(--background)] flex-1">
-					<CsvVisualizer />
-				</div>
-			</div>
-		</div>
-	);
+    useEffect(() => {
+        function onKeyDown(e: KeyboardEvent) {
+            if (e.key === "Escape") {
+                setIsSidebarOpen(false);
+            }
+        }
+        window.addEventListener("keydown", onKeyDown);
+        return () => window.removeEventListener("keydown", onKeyDown);
+    }, []);
+
+    const handleToggleSideBar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
+
+    const handleExit = () => {
+        loadView("Home");
+    }
+
+    return (
+        <div className="size-full">
+            <div className="project-dashboard-layout bg-[var(--secondary-color)] p-4 w-screen h-screen flex">
+                <div
+                    id="sidebar"
+                    className={`h-full ${isSidebarOpen ? "w-42" : "w-12"} bg-[var(--secondary-color)] transition-all duration-150 ease-in-out mr-3`}
+                >
+                    <div className="flex flex-col justify-between h-full">
+                        <div>
+                            <div className="flex items-center justify-start py-2">
+                                <button
+                                    type="button"
+                                    className="mr-2 p-1 rounded hover:bg-white/10 active:scale-95 transition-colors duration-150 ease-in-out focus:outline-none focus:ring-2 focus:ring-white/30"
+                                    onClick={handleToggleSideBar}
+                                    aria-label={isSidebarOpen ? "Collapse sidebar" : "Expand sidebar"}
+                                >
+                                    {isSidebarOpen ? (
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            className="h-6 w-6"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke="currentColor"
+                                        >
+                                            <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth={2}
+                                                d="M15 19l-7-7 7-7"
+                                            />
+                                        </svg>
+                                    ) : (
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            className="h-6 w-6"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke="currentColor"
+                                        >
+                                            <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth={2}
+                                                d="M10 6l6 6-6 6"
+                                            />
+                                        </svg>
+                                    )}
+                                </button>
+                                <p className="text-2xl truncate">
+                                    {isSidebarOpen ? (currentProject ? currentProject.name : "No project") : null}
+                                </p>
+                            </div>
+                            <div className="mt-8 flex flex-col gap-3 px-1">
+                                <button
+                                    type="button"
+                                    className="border border-white w-full text-xl rounded py-2 hover:bg-white/5 transition-colors duration-150 ease-in-out focus:outline-none focus:ring-2 focus:ring-white/20"
+                                    onClick={() => setSelectedTab("csv")}
+                                >
+                                    csv
+                                </button>
+                                <button
+                                    type="button"
+                                    className="border border-white w-full text-xl rounded py-2 hover:bg-white/5 transition-colors duration-150 ease-in-out focus:outline-none focus:ring-2 focus:ring-white/20"
+                                    onClick={() => setSelectedTab("tele")}
+                                >
+                                    tele
+                                </button>
+                            </div>
+                        </div>
+                        <button
+                            type="button"
+                            className="mb-2 border border-red-200 rounded transition-colors duration-150 ease-in-out hover:bg-red-600/10 focus:outline-none focus:ring-2 focus:ring-red-300"
+                            onClick={handleExit}
+                        >
+                            {isSidebarOpen ? (
+                                <div className="flex px-4 items-center gap-4">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 20 20"
+                                        width="24"
+                                        height="24"
+                                        fill="#FFFFFF"
+                                        style={{ opacity: 1 }}
+                                    >
+                                        <path d="M13 3v2h2v10h-2v2h4V3zm0 8V9H5.4l4.3-4.3l-1.4-1.4L1.6 10l6.7 6.7l1.4-1.4L5.4 11z" />
+                                    </svg>
+                                    <p className="text-[18px]">Exit</p>
+                                </div>
+                            ) : (
+                                <div className="flex items-center justify-center px-1">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 20 20"
+                                        width="24"
+                                        height="24"
+                                        fill="#FFFFFF"
+                                        style={{ opacity: 1 }}
+                                    >
+                                        <path d="M13 3v2h2v10h-2v2h4V3zm0 8V9H5.4l4.3-4.3l-1.4-1.4L1.6 10l6.7 6.7l1.4-1.4L5.4 11z" />
+                                    </svg>
+                                </div>
+                            )}
+                        </button>
+                    </div>
+                </div>
+                <div
+                    id="main-csv-content"
+                    className="w-full bg-[var(--background)] flex-1 transition-colors duration-150 ease-in-out"
+                >
+                    <CsvVisualizer />
+                </div>
+            </div>
+        </div>
+    );
 }
